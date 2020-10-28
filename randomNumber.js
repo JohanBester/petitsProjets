1844f1cd6ac19e15c368e0d87ac485768fc8c550
Johan Bester <johan.bester@yahoo.com>
2020-10-28 18:26:56 -0500

    Add random dates

--- randomNumber.js
+++
@@ -1,8 +0,0 @@
-// Generate a random number
-// Math.floor returns whole numbers
-function randomNumber(min = 1.1, max = 99.99) {
-  let numba = Math.floor(Math.random() * (max - min + 1) + min);
-  return numba;
-}
-
-console.log(randomNumber());
